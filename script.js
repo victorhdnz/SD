1a8c7f41bfd26053e0ef89a363a3fa1704a75e78
Victor Diniz <victorhugo10diniz@gmail.com>
2025-04-24 14:30:44 -0300

    Corrigindo caminho da imagem

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./Assets/avatar-light.png")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./Assets/avatar.png")
   }
 
   if (html.classList.contains("light")) {
